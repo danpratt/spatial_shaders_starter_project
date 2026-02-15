376894ec51ecc27a0e43710c72972134a5cbf969
Daniel Pratt <daniel@blaumagier.com>
2026-02-15 16:25:49 -0500

    feat: finish course

--- scripts/spatial_shaders.gd
+++ scripts/spatial_shaders.gd
@@ -1,7 +1,17 @@
 extends Node3D
 
+@onready var sphere = $SphereExample
+
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-
+		
+	if Input.is_action_just_pressed("random_color"):
+		var sphere_material = sphere.get_active_material(0)
+		var random_color = getRandomColor()
+		sphere_material.set_shader_parameter("sphere_color", random_color)
+	
+	
+func getRandomColor():
+	return Vector3(randf(), randf(), randf())
+	
